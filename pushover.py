a825713588b41281e3ab4f7e98176ff9cd10a547
Alex Rudy <opensource@alexrudy.net>
2021-03-23 19:36:27 -0700

    Bump version: 0.3.0 → 0.4.0

--- pushover.py
+++ pushover.py
@@ -14,7 +14,7 @@ Copyright 2017 Alexander Rudy. All rights reserved.
 __author__ = """Alexander Rudy"""
 __email__ = "pushover@alexrudy.net"
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 
 import sys
 import os
